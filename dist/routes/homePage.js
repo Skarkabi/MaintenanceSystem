3876438de4a912a35d7fd1f5a326f9a2dd94f206
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2021-07-27 13:19:01 +0400

    Added comments to homepage route

--- dist/routes/homePage.js
+++ dist/routes/homePage.js
@@ -7,65 +7,25 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
-
-var _asyncToGenerator2 = _interopRequireDefault(require("@babel/runtime/helpers/asyncToGenerator"));
-
 var _express = _interopRequireDefault(require("express"));
 
-var _passport = _interopRequireDefault(require("passport"));
-
-var _bluebird = _interopRequireDefault(require("bluebird"));
-
-var _User = _interopRequireDefault(require("../models/User"));
-
-var _fs = _interopRequireDefault(require("fs"));
-
-//import { Authenticated, IsAdmin, IsStudent, IsOwnPage } from '../authentication';
-//import { body, validationResult } from 'express-validator';
 var router = _express["default"].Router();
 /** 
- * Displays login page.
+ * Express Route to Displays login page.
  */
 
 
-var mime = {
-  html: 'text/html',
-  txt: 'text/plain',
-  css: 'text/css',
-  gif: 'image/gif',
-  jpg: 'image/jpeg',
-  png: 'image/png',
-  svg: 'image/svg+xml',
-  js: 'application/javascript'
-};
-router.get('/', /*#__PURE__*/function () {
-  var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res, next) {
-    return _regenerator["default"].wrap(function _callee$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            if (req.user) {
-              res.render('dashboardForAdmins', {
-                title: 'Home Page',
-                jumbotronDescription: "Welcome! This is your dashboard and you can access everything from here easily.",
-                msgType: req.flash()
-              });
-            } else {
-              res.redirect('/login');
-            }
-
-          case 1:
-          case "end":
-            return _context.stop();
-        }
-      }
-    }, _callee);
-  }));
-
-  return function (_x, _x2, _x3) {
-    return _ref.apply(this, arguments);
-  };
-}());
+router.get('/', function (req, res, next) {
+  //Checking if a user is already logged in
+  if (req.user) {
+    res.render('dashboardForAdmins', {
+      title: 'Home Page',
+      jumbotronDescription: "Welcome! This is your dashboard and you can access everything from here easily.",
+      msgType: req.flash()
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
 var _default = router;
 exports["default"] = _default;
--- server/routes/homePage.js
+++ server/routes/homePage.js
@@ -1,41 +1,25 @@
 import express from 'express';
-import passport from 'passport';
-import Bluebird from 'bluebird';
-//import { Authenticated, IsAdmin, IsStudent, IsOwnPage } from '../authentication';
-//import { body, validationResult } from 'express-validator';
-import User from '../models/User';
-import fs from 'fs'
 
 const router = express.Router();
 
 /** 
- * Displays login page.
+ * Express Route to Displays login page.
  */
+router.get('/', (req, res, next) => {
+    //Checking if a user is already logged in
+    if (req.user){
+        res.render('dashboardForAdmins', 
+            {title: 'Home Page',
+            jumbotronDescription: "Welcome! This is your dashboard and you can access everything from here easily.",
+            msgType: req.flash() 
 
+        });
 
- var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
-};
-
-router.get('/', async (req, res, next) => 
-{
-    if (req.user)
-    {
-        res.render('dashboardForAdmins', {title: 'Home Page',
-        jumbotronDescription: "Welcome! This is your dashboard and you can access everything from here easily.",
-        msgType: req.flash() });
-    }
-    else
-    {
+    }else{
        res.redirect('/login');
+
     }
+
 });
 
 export default router;

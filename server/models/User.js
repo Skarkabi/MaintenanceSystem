f32515433cfa767a5bbc1339836eabb2c080ee73
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2021-07-27 12:14:14 +0400

    Added comments to User model

--- server/models/User.js
+++ server/models/User.js
@@ -2,11 +2,12 @@ import _ from 'lodash';
 import bcrypt from 'bcrypt';
 import Bluebird from 'bluebird';
 import Sequelize from 'sequelize';
-import multer from 'multer';
 import fs from 'fs';
-
 import sequelize from '../mySQLDB';
 
+/**
+ * Declaring the datatypes used within the User class
+ */
 const mappings = {
   id: {
     type: Sequelize.DataTypes.STRING,
@@ -48,6 +49,9 @@ const mappings = {
   }
 };
 
+/**
+ * Defining the user table within the MySQL database using Sequelize
+ */
 const User = sequelize.define('User', mappings, {
   indexes: [
     {
@@ -104,6 +108,7 @@ const User = sequelize.define('User', mappings, {
  */
 
 User.createUser = (createdUser) => {
+  //Declaring the new User variable to be added
   const newUser =  
   {
 
@@ -118,26 +123,35 @@ User.createUser = (createdUser) => {
   
 
   return new Bluebird((resolve, reject) => {
+     //using bcrypt to encrypt the password for the user before saving
      bcrypt.genSalt(10, function (err, salt)
      {
       bcrypt.hash(newUser.password, salt, function (e, hash)
       {
         if(e) reject (e);
+        //Checking if the user id already exists
         User.getUserById(newUser.id).then(isUserRegestered => 
           {
+          //If user id exists reject user input
           if(isUserRegestered){
             reject ("Employee with ID# " + newUser.id+ " Already Registered");
 
           }else{
+            //Checking if user name already exists
             User.getUserByUserName(newUser.username).then(isUser =>
               {
+              //If user name already exists reject user input
               if(isUser){
                 reject ("Username " + newUser.username + " Already Taken");
 
+              //If user name and id do not exist add new user to database
               }else{
                 newUser.password = hash;
                 User.create(newUser).then(() => {
                   resolve("Emplyee With ID# " + newUser.id + " Was Sucessfully Added!");
+
+                }).catch(err => {
+                  reject(err);
 
                 });
 
@@ -157,14 +171,22 @@ User.createUser = (createdUser) => {
 
 }
 
+/**
+ * Funciton to delete user from database
+ * @param {*} id 
+ * @returns msg to flash to user
+ */
 User.deleteUserById = id => {
   return new Bluebird((resolve, reject) => {
+    //Check if user exists
     User.getUserById(id).then(foundUser => {
+      //If exists delete from database
       foundUser.destroy();
       resolve("User with Employee ID# " + id + " Was Sucessfully Removed From the System!");
     
     }).catch(err => {
-      reject("An Error has Occured User with Employee ID# " + id + " Could not be Deleted");
+      //If dosent exist inform user error occured
+      reject("An Error has Occured User with Employee ID# " + id + " Could not be Deleted " + err);
 
     });
 
@@ -173,21 +195,40 @@ User.deleteUserById = id => {
 }
 
 
-
+/**
+ * Function to find user by ID
+ * @param {*} id 
+ * @returns found user
+ */
 User.getUserById = id => User.findOne({
     where:{id},
   });
 
 
+/**
+ * Function to find user by first name
+ * @param {*} firstName 
+ * @returns  found user
+ */
 User.getUserByFirstName = firstName => User.findOne({
   where: {firstName},
 });
 
+/**
+ * Function to find user by username
+ * @param {*} userName 
+ * @returns found user
+ */
 User.getUserByUserName = userName => User.findOne({
   where: {userName},
 });
 
 
+/**
+ * Function to decypt password and check if it is a match
+ * @param {*} password 
+ * @returns false if password does not match
+ */
 User.prototype.comparePassword = function (password) {
   return Bluebird.resolve().then(() => 
     bcrypt.compare(password, this.password)).catch(
@@ -200,22 +241,35 @@ User.prototype.comparePassword = function (password) {
 
 };
 
+/**
+ * Fucntion to update the users profile picture
+ * Function saves image to server and sets the image name
+ * @param {*} userId 
+ * @param {*} username 
+ * @param {*} profilePicture 
+ */
 User.updateProfilePhoto = (userId, username, profilePicture) => {
+  //Converting profile picture object to string to be uploaded 
   profilePicture = JSON.stringify(profilePicture).slice(23);
-  console.log("int here");
-    fs.writeFileSync(`./public/profilePictures/${username}.jpeg`, new Buffer.from(profilePicture, 'base64', (err) => {
-        if (err) return console.error(err)
-         console.log('file saved to ', '/mytest2.png')
-    }))
-      console.log("didnt break yet");
-      User.getUserById(userId).then(toUpdate => {
-        toUpdate.profilePicture = `${username}.jpeg`;
-        toUpdate.save();
-      }).catch(err => {
-        console.log("This happened " + err);
-      });
-    }
 
+  //Saving profile picture to server
+  fs.writeFileSync(`./public/profilePictures/${username}.jpeg`, new Buffer.from(profilePicture, 'base64', (err) => {
+    if (err) return console.error(err)
+  
+  }));
 
+  //Getting the user from database who's profile picture will be updated
+  User.getUserById(userId).then(toUpdate => {
+    //Updating profile picture name in database
+    toUpdate.profilePicture = `${username}.jpeg`;
+    //Saving update in database
+    toUpdate.save();
+
+  }).catch(err => {
+      console.log("This happened " + err);
+
+  });
+
+}
 
 export default User;

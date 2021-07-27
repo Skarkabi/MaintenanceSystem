c9e9766bcd28411de03a43144be11b510cdaf54b
Skarkabi <saleemkarkabi@cmail.carleton.ca>
2021-07-27 09:16:34 +0400

    Added Comments for Filter Model

--- server/models/consumables/Filter.js
+++ server/models/consumables/Filter.js
@@ -5,6 +5,9 @@ import sequelize from '../../mySQLDB';
 import Bluebird from 'bluebird';
 import Supplier from '../Supplier';
 
+/**
+ * Declaring the datatypes used within the Filter class
+ */
 const mappings = {
     id: {
         type: Sequelize.INTEGER,
@@ -77,19 +80,22 @@ const mappings = {
     },
 };
 
+/**
+ * Defining the filter stocks table within the MySQL database using Sequelize
+ */
 const Filter = sequelize.define('filter_stocks', mappings, {
     
     indexes: [
     {
-      name: 'filter_id_index',
-      method: 'BTREE',
-      fields: ['id'],
+        name: 'filter_id_index',
+        method: 'BTREE',
+        fields: ['id'],
     },
     
     {
-      name: 'filter_quantity_index',
-      method: 'BTREE',
-      fields: ['quantity'],
+        name: 'filter_quantity_index',
+        method: 'BTREE',
+        fields: ['quantity'],
     },
     
     {
@@ -149,14 +155,14 @@ const Filter = sequelize.define('filter_stocks', mappings, {
         fields: ['minQuantity'],
     },
     {
-      name: 'filter_createdAt_index',
-      method: 'BTREE',
-      fields: ['createdAt'],
+        name: 'filter_createdAt_index',
+        method: 'BTREE',
+        fields: ['createdAt'],
     },
     {
-      name: 'filter_updatedAt_index',
-      method: 'BTREE',
-      fields: ['updatedAt'],
+        name: 'filter_updatedAt_index',
+        method: 'BTREE',
+        fields: ['updatedAt'],
     },
     {
         name: 'filter_supplierId_index',
@@ -171,17 +177,27 @@ const Filter = sequelize.define('filter_stocks', mappings, {
   ],
 });
 
+/**
+ * Function to update Filter stock
+ * Takes in the filter object and if the value should be deleted or added
+ * @param {*} newFilter 
+ * @param {*} action 
+ * @returns msg to be flashed to user
+ */
 Filter.updateFilter = (newFilter, action) => {
     return new Bluebird((resolve, reject) => {
+        //Creating the new Consumable value to be updated in the consumable databse
         const newConsumable = {
             category: "Filter",
             quantity: newFilter.quantity
         };
 
+        //Checking if the filter spec exists within the stock
         Filter.findOne({
             where: {id: newFilter.id}
 
         }).then(foundFilter => {
+            //If the filter exists in the databse the function sets the new quantity to the new value
             var quant;
             if(action === "add"){
                quant = parseInt(newFilter.quantity) + foundFilter.quantity;
@@ -191,6 +207,7 @@ Filter.updateFilter = (newFilter, action) => {
 
             }
 
+            //If the new value is 0 the filter definition is deleted from the stock
             if(quant === 0){
                 foundFilter.destroy().then(() => {
                     resolve("Filter Completly Removed From Stock!");
@@ -200,16 +217,20 @@ Filter.updateFilter = (newFilter, action) => {
 
                 });
 
+            //If the new quantity is less than 0 rejects the user input
             }else if(quant < 0){
                 reject("Can Not Delete More Than Exists in Stock!");
     
+            //If quantity is > 0 the brake quantity is updated
             }else{
+                //looking for the filter to update and setting the new quantity
                 Filter.update({quantity:quant}, {
                     where: {
                         id: newFilter.id
                     }
     
                 }).then(() => {
+                    //Updating the value from the consumables database
                     Consumable.updateConsumable(newConsumable, action).then(() => {
                         if(action === "add"){
                             resolve(newFilter.quantity + " Fitlers Sucessfully Added to Existing Stock!");
@@ -221,19 +242,19 @@ Filter.updateFilter = (newFilter, action) => {
                         
     
                     }).catch(err => {
-                        reject("An Error Occured Filters Stock Could not be Updated");
+                        reject("An Error Occured Filters Stock Could not be Updated (Error: " + err + ")");
     
                     });
     
                 }).catch(err => {
-                    reject("An Error Occured Filters Stock Could not be Updated");
+                    reject("An Error Occured Filters Stock Could not be Updated (Error: " + err + ")");
     
                 });
 
             }
             
         }).catch(err => {
-            reject("An Error Occured Filters Stock Could not be Updated");
+            reject("An Error Occured Filters Stock Could not be Updated (Error: " + err + ")");
 
         });
 
@@ -241,12 +262,21 @@ Filter.updateFilter = (newFilter, action) => {
 
 }
 
+/**
+ * Function to add a new Filter into stock
+ * Function takes an object with the needed filter info
+ * @param {*} newFilter 
+ * @returns msg to be flashed to user
+ */
 Filter.addFilter = (newFilter) => {
     return new Bluebird((resolve, reject) => {
+        //Creating the new Consumable value to be updated in the consumable databse
         const newConsumable = {
             category: "Filter",
             quantity: newFilter.quantity
         };
+
+        //Looking if the filter with exact specs and same quotation number already exists in stock
         Filter.findOne({
             where: {
                 carBrand: newFilter.carBrand,
@@ -259,16 +289,20 @@ Filter.addFilter = (newFilter) => {
             }
 
         }).then(foundFilter => {
+            //If this filter with this quotation number exists the function rejects the creation
             if(foundFilter){
                 reject("Filters With these Details Already Registered, Please Add to Existing Stock");
 
+            //If the filter is not found the function creates a filter and updates the consumable stock
             }else{
+                //Converting values to appropiate number type
                 newFilter.quantity = parseInt(newFilter.quantity);
                 newFilter.minQuantity = parseInt(newFilter.minQuantity);
                 newFilter.singleCost = parseFloat(newFilter.singleCost);
                 newFilter.totalCost = newFilter.singleCost * newFilter.quantity;
                 newFilter.totalCost = parseFloat(newFilter.totalCost);
                 Filter.create(newFilter).then(() => {
+                    //Updating consumable stock database
                     Consumable.updateConsumable(newConsumable, "add").then(() => {
                         resolve(newFilter.quantity + " Filters Sucessfully Added!");
 
@@ -292,10 +326,17 @@ Filter.addFilter = (newFilter) => {
 
 }
 
+/**
+ * Function to set virtual datatype supplier name using supplier ids in filter
+ * @returns List of filters with their supplier names
+ */
 Filter.getStock = () => {
     return new Bluebird ((resolve, reject) => {
+        //Getting all the brakes found in the database
         Filter.findAndCountAll().then(filters => {
+            //Calling supplier function to add supplier name to brake objects
             Supplier.getSupplierNames(filters).then(() => {
+                //returning the brakes 
                 resolve(filters);
 
             }).catch(err => {
@@ -312,17 +353,30 @@ Filter.getStock = () => {
 
 }
 
+/**
+ * function to return distinct values in object
+ * @param {*} values 
+ * @returns filtered values 
+ */
 function getDistinct(values){
     return values.filter((value, index, self) => self.indexOf(value) === index);
 }
 
+/**
+ * Function to return list of fitlers with their supplier names, as well as distinct values found within the database
+ * @returns object that includes all filters, suppliers, and distinct values of each filter spec
+ */
 Filter.getFilterStock = () => {
     return new Bluebird(async (resolve, reject) => {
+        //Declaring all variables to be returned
         var filterC, filterS, typeF, carBrand, carModel, carYear, preferredBrand, carCategory, singleCost, actualBrand;
+        //Getting all suppliers saved in database
         Supplier.findAll().then(suppliers => {
             filterS = suppliers
+            //Getting all filters from database and setting supplier names 
             Filter.getStock().then(consumables => {
                 filterC = consumables;
+                //Mapping filter values to not return double values
                 carCategory = getDistinct(filterC.rows.map(val => val.category));
                 typeF = getDistinct(filterC.rows.map(val => val.fType));
                 carBrand = getDistinct(filterC.rows.map(val => val.carBrand));
@@ -332,6 +386,7 @@ Filter.getFilterStock = () => {
                 actualBrand = getDistinct(filterC.rows.map(val => val.actualBrand));
                 singleCost = getDistinct(filterC.rows.map(val => val.singleCost));
 
+                //Creating variable of all need variables to return
                 var values = {
                     consumable: filterC.rows, suppliers: filterS,filterType: typeF, carBrand: carBrand, 
                     carModel: carModel, carYear: carYear, preferredBrand: preferredBrand, carCategory: carCategory,
@@ -355,37 +410,73 @@ Filter.getFilterStock = () => {
 
 }
 
+/**
+ * Function to find all filters in database with specific supplier ID
+ * Function takes in the supplier ID of the supplier being searched for
+ * @param {*} supplierId 
+ * @returns List of filters purchased from that specified supplier ID
+ */
 Filter.getWithSupplier = supplierId => {
     return new Bluebird((resolve, reject) => {
+        //Finding all filters with specified supplier ID
         Filter.findAndCountAll({
             where: {
                 supplierId: supplierId
             }
-        }).then(async foundFilter => {
-            await Supplier.getSupplierNames(foundFilter);
-            resolve(foundFilter.rows);
+        }).then(foundFilter => {
+            //Adding supplier Name to filters 
+            Supplier.getSupplierNames(foundFilter).then(() => {
+                resolve(foundFilter.rows);
+            
+            }).catch(err => {
+                reject(err);
+
+            });
+            
         }).catch(err => {
             reject(err);
+
         });
+
     });
+
 }
 
+/**
+ * Function to find different filters from a specific suppleir 
+ * @returns list of filters values from specific supplier regardless of quotation numbers
+ */
 Filter.groupSupplier = () => {
     return new Bluebird((resolve, reject) => {
+        //Finding filters from database and returning specified attributes 
         Filter.findAll({
+            //Declaring attributes to return from database
             attributes:
               ['category', 'fType', 'actualBrand', 'carModel', 'carBrand', 'carYear', 'singleCost', 'supplierId',
               [sequelize.fn('sum', sequelize.col('quantity')), 'quantity'],
             ],
 
+            //Declaring how to group return values
             group: ['category', 'fType', 'actualBrand', 'carModel', 'carBrand', 'carYear', 'singleCost', 'supplierId']
             
         }).then(async (values) => { 
+            //Setting variable to return filters with their supplier names
             var result = {count: values.length, rows: values}
-            await Supplier.getSupplierNames(result);
-           resolve(result);
+            Supplier.getSupplierNames(result).then(() => {
+                resolve(result);
+
+            }).catch(err => {
+                reject(err);
+
+            });
+           
+        }).catch(err => {
+            reject(err);
+
         });
-    })
+
+    });
+    
 }
 
 
